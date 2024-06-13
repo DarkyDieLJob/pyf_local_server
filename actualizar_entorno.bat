abba1dd68fbb729536a81c7c6b8e1b45a78f75e7
Matías Leandro López <diel.job@gmail.com>
2024-06-12 21:19:32 -0300

    corrijiendo .bats

--- actualizar_entorno.bat
+++ actualizar_entorno.bat
@@ -1,7 +1,13 @@
 @echo off
 cd %~dp0
+
+IF NOT EXIST "venv\" (
+    echo Creando entorno virtual...
+    python -m venv venv
+)
+
 call venv\Scripts\activate
-python.exe -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 echo.
 echo ¡Actualización completada!
--- run_server.bat
+++ run_server.bat
@@ -3,4 +3,5 @@ cd %~dp0
 call venv\Scripts\activate
 cd local_server
 python manage.py runserver 192.168.1.104:8080
+python manage.py runserver 192.168.0.15:8080
 pause
